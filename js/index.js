e216b526cb548acccf52d0069790bac1894a06ed
Benjamin Broadbent <benjamin.d.broadbent@gmail.com>
2022-02-18 09:41:29 +0000

    latest code

--- js/index.js
+++ js/index.js
@@ -5,13 +5,28 @@
 
 // //methods
 // fullpage_api.setAllowScrolling(false);
+
+let myFullpage = null;
 $(document).ready(function() {
-    var myFullpage = new fullpage('#fullpage', {
+    myFullpage = new fullpage('#fullpage', {
         licenseKey: 'C5F2D660-6E6F47CC-944F6D71-9819109F',
         sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
         anchors: ['about-me', 'my-projects', 'contact-me'],
         navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips: ['First page', 'Second page', 'Third and last page']
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+        afterLoad: function(origin, destination, direction) {
+            if (myFullpage) {
+                console.log('Hi')
+                if (myFullpage.getActiveSlide()) {
+                    $('#fp-nav').css('visibility', 'hidden')
+                    $('#fp-nav').css('opacity', '0')
+                } else {
+                    $('#fp-nav').css('visibility', 'unset')
+                    $('#fp-nav').css('opacity', '1')
+                }
+            }
+        }
     });
 })
